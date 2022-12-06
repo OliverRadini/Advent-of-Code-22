667e9e286d13209f348d4c6312c995d407d0bfea
Oliver Radini <orr2@live.com>
2022-12-06 23:16:01 +0000

    5 updated, now working

--- 5.js
+++ 5.js
@@ -1,5 +1,5 @@
+const { assert } = require("console");
 const { readFile } = require("fs");
-
 
 const groupBy = f => xs => xs
     .reduce(
@@ -16,53 +16,47 @@ const groupBy = f => xs => xs
         {}
     );
 
-const mapValues = f => o => Object.keys(o).reduce(
-    (p, c) => ({
-        ...p,
-        [c]: f(o[c]),
-    }),
-    {}
-);
-
 const isMove = s => s[0] === "m";
 
 const parseInputFile = raw => groupBy(x => isMove(x) ? "move" : "stack")(raw.split("\n"));
 
 const parseRawMove = move => {
     const [_, amount, start, finish] = /(\d+).+(\d+).+(\d+)/.exec(move);
-    return {amount, start, finish};
+    return {amount: Number(amount), start: Number(start), finish: Number(finish) };
 };
 
-const COLUMN_SIZE = 4;
-const DATA_INDEX = 1;
-const EMPTY_SPACE = "-";
-const parseRawStackLine = rawLine => Array.from(rawLine)
-    .reduce(
-        (p, c, i) => {
-            const columnPart = i % COLUMN_SIZE;
-            const isData = columnPart === DATA_INDEX;
+const applyMove = (s, { amount, start, finish }) => {
+    const itemsToAdd = s[start].slice(-amount);
+    const newState = {
+        ...s,
+        [start]: s[start].slice(0, -amount),
+        [finish]: [...s[finish], ...itemsToAdd]
+    };
+    const currentSize = Object.keys(s).map(k => s[k].length).reduce((p, c) => p + c, 0)
+    const newSize = Object.keys(newState).map(k => newState[k].length).reduce((p, c) => p + c, 0)
 
-            if (isData) {
-                p.push(c === " " ? EMPTY_SPACE : c)
-            }
-            return p;
-        },
-        []
-    )
-    .map((x, i) => ({ col: i, item: x }));
+    assert(currentSize === newSize);
 
-const parseRawStack = raws => mapValues(x => x.map(y => y.item).filter(x => x !== EMPTY_SPACE))(
-    groupBy(x => x.col)(
-        raws
-            .map(parseRawStackLine)
-            .filter(x => x.length > 0)
-            .slice(0, -1)
-            .flat()
-    )
-);
+    return newState;
+};
 
-const applyMove = (s, m) => {
-     // TODO: implement this
+const parseRawStack = rawStack => {
+    const width = rawStack.reduce((p, c) => c.length > p ? c.length : p, 0);
+
+    const result = Array.from(Array(width))
+        .map((_, i) => rawStack.map(x => x[i]))
+        .map(stack => stack.filter(x => !/[\[\] ]/g.test(x)).filter(x => x !== undefined))
+        .filter(x => x.length > 0)
+        .map(x => x.reverse())
+        .reduce(
+            (p, c) => ({
+                ...p,
+                [c[0]]: c.slice(1)
+            }),
+            {}
+        )
+
+    return result;
 };
 
 readFile("./data5", "utf8", (err, data) => {
@@ -74,6 +68,13 @@ readFile("./data5", "utf8", (err, data) => {
     const { move: rawMoves, stack: rawStack } = parseInputFile(data);
 
     const parsedMoves = rawMoves.map(parseRawMove);
-    const parsedStack = parseRawStack(rawStack);
-    console.log(parsedStack);
+    let parsedStack = parseRawStack(rawStack);
+
+    const rearrangedStack = parsedMoves
+        .reduce(
+            (p, c) => applyMove(p, c),
+            parsedStack,
+        );
+
+    console.log(Object.values(rearrangedStack).map(x => x[x.length - 1]).join(""));
 });
