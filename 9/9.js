62bc8679b8aac90464dd76ed69374c9be7778cd4
Oliver Radini <orr2@live.com>
2022-12-11 23:07:55 +0000

    9 done

--- 9/9.js
+++ 9/9.js
@@ -1,17 +1,36 @@
 const { readFileSync } = require("fs");
 
+const TAIL_COUNT = 9;
+
 const parseInputToActions = input => input
     .split("\n")
     .map(line => line.split(" "))
     .map(([dir, n]) => Array.from(Array(Number(n))).map(() => ({ type: dir })))
     .flat();
 
-const initialState = {
-    tailX: 0,
-    tailY: 0,
-    headX: 0,
-    headY: 0,
+const movementReducer = (state, action) => {
+    const clone = [...state];
+    switch (action.type) {
+        case "U":
+            clone[0] = [state[0][0], state[0][1] - 1];
+            return clone;
+        case "D":
+            clone[0] = [state[0][0], state[0][1] + 1];
+            return clone;
+        case "L":
+            clone[0] = [state[0][0] + 1, state[0][1]];
+            return clone;
+        case "R":
+            clone[0] = [state[0][0] - 1, state[0][1]];
+            return clone;
+        case "TAIL":
+            return moveTail(state, action.payload);
+        default:
+            return initialState;
+    }
 };
+
+const initialState = Array.from(Array(TAIL_COUNT + 1)).map(() => [0, 0]);
 
 const pointComparison = (a, b) => {
     if (a < b) {
@@ -25,48 +44,42 @@ const pointComparison = (a, b) => {
     return 0;
 }
 
-const moveTail = (state) => {
+const moveTail = (state, tailNumber) => {
+    if (tailNumber === 0) {
+        throw new Error("Cannot move head via moveTail function");
+    }
+
+    const [pX, pY] = state[tailNumber];
+    const [hX, hY] = state[tailNumber - 1];
+
     if (
-        Math.abs(state.headX - state.tailX) <= 1
-        && Math.abs(state.headY - state.tailY)  <= 1
+        Math.abs(hX - pX) <= 1
+        && Math.abs(hY - pY)  <= 1
     ) {
         return state;
     }
 
-    const tailXDirection = pointComparison(state.headX, state.tailX);
-    const tailYDirection = pointComparison(state.headY, state.tailY);
+    const tailXDirection = pointComparison(hX, pX);
+    const tailYDirection = pointComparison(hY, pY);
 
-    return {
-        ...state,
-        tailX: state.tailX + tailXDirection,
-        tailY: state.tailY + tailYDirection,
-    };
+    const pointsClone = [...state];
+    pointsClone[tailNumber] = [pX + tailXDirection, pY + tailYDirection];
+
+    return pointsClone;
 };
 
-const movementReducer = (state, action) => {
-    switch (action.type) {
-        case "U":
-            return { ...state, headY: state.headY + 1 };
-        case "D":
-            return { ...state, headY: state.headY - 1 };
-        case "L":
-            return { ...state, headX: state.headX - 1 };
-        case "R":
-            return { ...state, headX: state.headX + 1 };
-        case "TAIL":
-            return moveTail(state);
-        default:
-            return initialState;
-    }
-};
 
 const printGrid = (size, state) => {
     for (let y = 0; y < size; y++) {
         let line = "";
         for (let x = 0; x < size; x++) {
-            const isHeadMatch = x === state.headX && y === state.headY;
-            const isTailMatch = x === state.tailX && y === state.tailY;
-            const symbol = isHeadMatch ? "H" : isTailMatch ? "T" : ".";
+            let matchingIndex = null;
+            state.forEach(([pX, pY], i) => {
+                if (pX === x && pY === y) {
+                    matchingIndex = i;
+                }
+            });
+            const symbol = matchingIndex === null ? "-" : `${matchingIndex}`;
             line = `${line}${symbol}`;
         }
         console.log(line);
@@ -75,16 +88,18 @@ const printGrid = (size, state) => {
 
 
 let data = readFileSync("./data9", "utf8");
-
 const actions = parseInputToActions(data);
+
 
 const visitedPoints = {};
 let state = initialState;
 for (let i = 0; i < actions.length; i++) {
     state = movementReducer(state, actions[i]);
-    visitedPoints[`${state.tailX}|${state.tailY}`] = true;
-    state = movementReducer(state, { type: "TAIL" });
-    visitedPoints[`${state.tailX}|${state.tailY}`] = true;
+    for (let a = 1; a < state.length; a++) {
+        state = movementReducer(state, { type: "TAIL", payload: a });
+    }
+    const [tX, tY] = state[state.length - 1];
+    visitedPoints[`${tX}|${tY}`] = true;
 }
 
 const result = Object.keys(visitedPoints).length;
