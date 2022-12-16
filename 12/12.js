b1d8dccda5045ac1944479dd88f5bd6379a6968d
Oliver Radini <orr2@live.com>
2022-12-16 12:31:39 +0000

    12 part 1 done

--- 12/12.js
+++ 12/12.js
@@ -1,7 +1,7 @@
 const { readFileSync } = require("fs");
 
 const letterLookup = { S: 1, E: 26, a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10, k: 11, l: 12, m: 13, n: 14, o: 15, p: 16, q: 17, r: 18, s: 19, t: 20, u: 21, v: 22, w: 23, x: 24, y: 25, z: 26 };
-const filename = process.argv[2] || "./data12";
+const filename = process.argv[2] || "./data12_actual";
 const input = readFileSync(filename, "utf8")
     .split("\n")
     .map(line => line.split(""));
@@ -23,6 +23,11 @@ const findValue = (value) => {
 const startingCoord = findValue("S");
 const endingCoord = findValue("E");
 
+const tempCheckCoord = {
+    x: endingCoord.x + 7,
+    y: endingCoord.y,
+};
+
 if (startingCoord === null || endingCoord === null) {
     throw new Error("Can't find start/end");
 }
@@ -36,28 +41,41 @@ const getValueAt = (at) => {
     }
 };
 
-const getCoordinatesSurrounding = ({ x: xFrom, y: yFrom }) => [
-    { x: xFrom, y: yFrom + 1 },
-    { x: xFrom, y: yFrom - 1 },
-    { x: xFrom - 1, y: yFrom },
-    { x: xFrom + 1, y: yFrom },
-]
-    .filter(c => {
-        try {
-            const isGreaterThanZero = c.x >= 0 && c.y >= 0;
+const getCoordinatesSurrounding = ({ x: xFrom, y: yFrom }) => {
+    if (xFrom === tempCheckCoord.x && yFrom === tempCheckCoord.y) {
+        // console.log("Getting surrounding for temp checking coord");
+    }
+    const allCoords = [
+        { x: xFrom, y: yFrom + 1 },
+        { x: xFrom, y: yFrom - 1 },
+        { x: xFrom - 1, y: yFrom },
+        { x: xFrom + 1, y: yFrom },
+    ];
 
-            if (!isGreaterThanZero) {
-                return false;
+    return allCoords.filter(
+        c => {
+                try {
+                    const isGreaterThanZero = c.x >= 0 && c.y >= 0;
+
+                    if (!isGreaterThanZero) {
+                        return false;
+                    }
+                    
+                    const width = input[c.y].length;
+                    const isWithinBounds = c.y < HEIGHT && c.x < width;
+
+                    if (c.x === tempCheckCoord.x && c.y === tempCheckCoord - 1) {
+                        // console.log(`isWithinBounds? ${isWithinBounds}`);
+                    }
+
+                    return isWithinBounds;
+                } catch (error) {
+                    // console.log(`Error finding coordinate (${c.x}, ${c.y})`);
+                    return false;
+                }
             }
-            
-            const isWithinBounds = c.y < HEIGHT && c.x < input[c.y].length;
-
-            return isWithinBounds;
-        } catch (error) {
-            console.log(`Error finding coordinate (${c.x}, ${c.y})`);
-            return false;
-        }
-    });
+        )
+};
 
 const coordHeightCache = {};
 
@@ -75,9 +93,23 @@ let allNodes = input
 const getAllNodeAtCoord = (coord) => allNodes[coord.y][coord.x];
 
 const getNodeschildren = node => {
-    return getCoordinatesSurrounding(node)
-        .map(getAllNodeAtCoord)
-        .filter(coord => coord && walkIsViable(node.v)(coord.v))
+    if (node.x === tempCheckCoord.x && node.y === tempCheckCoord.y) {
+        // console.log("Getting children for temp checking coord");
+    }
+    // TODO: make sure we're not using map/filter here as it'll return a copy
+    const surrounding = getCoordinatesSurrounding(node);
+    const allNodes = surrounding.map(getAllNodeAtCoord);
+    const nodesToReturn = []
+    for (let i = 0; i < allNodes.length; i++) {
+        const thisNode = allNodes[i]
+        if (
+            !!thisNode
+                && walkIsViable(node.v)(thisNode.v)
+        ) {
+            nodesToReturn.push(thisNode);
+        }
+    }
+    return nodesToReturn;
 }
 
 const knownWalkLengths = {};
@@ -85,6 +117,9 @@ const knownWalkLengths = {};
 for (let y = 0; y < allNodes.length; y++) {
     const thisLine = allNodes[y];
     for (let x = 0; x < thisLine.length; x++) {
+        if (x === tempCheckCoord.x && y === tempCheckCoord.y) {
+            // console.log("About to check for children of temp checking coord");
+        }
         allNodes[y][x].children = getNodeschildren(allNodes[y][x]);
         allNodes[y][x].children.forEach(child => {
             child.parents.push(allNodes[y][x]);
@@ -105,37 +140,39 @@ const calculateWalkCost = (startX, startY, endX, endY) => {
 
     const children = allNode.children
         .filter(x => Number.isFinite(x.cost))
-        .filter(x => x.cost !== -1)
-        .filter(x => x.cost !== -7);
+        .filter(x => x.cost !== -1);
 
     if (children.length === 0) {
-        return -7;
+        return -1;
     }
 
     if (children.every(x => x.cost === -1)) {
-        return -2;
+        return -1;
     }
 
-    return Math.min(...children.map(x => x.cost)) + 1;
+    return Math.min(...children.filter(x => x.cost >= 0).map(x => x.cost)) + 1;
 };
 
-const tempPrintingSize = 20;
+const tempPrintingSize = 10;
 
-for (let i = 0; i < 10000; i++) {
+for (let i = 0; i < 100000; i++) {
     for (let y = 0; y < allNodes.length; y++) {
         const thisLine = allNodes[y];
         for (let x = 0; x < thisLine.length; x++) {
+            if (x === tempCheckCoord.x && y === tempCheckCoord.y) {
+                // console.log("About to check for children of temp checking coord");
+            }
             allNodes[y][x].cost = calculateWalkCost(x, y, endingCoord.x, endingCoord.y)
         }
     }
 
 }
-    const surroundingEnd = allNodes.slice(endingCoord.y - tempPrintingSize, endingCoord.y + tempPrintingSize)
-        .map(line => line.slice(endingCoord.x - tempPrintingSize, endingCoord.x + tempPrintingSize).map(x => x.cost < 0 ? "-" : x.cost).join("|")).join("\n")
+const surroundingEnd = allNodes.slice(endingCoord.y - tempPrintingSize, endingCoord.y + tempPrintingSize)
+    .map(line => line.slice(endingCoord.x - tempPrintingSize, endingCoord.x + tempPrintingSize).map(x => x.cost < 0 ? "---" : `${x.cost}`.padStart(3, " ")).join("|")).join("\n")
 
-    console.log("=============================")
-    console.log(surroundingEnd);
-    console.log("=============================")
+console.log("=============================")
+console.log(surroundingEnd);
+console.log("=============================")
 
 // console.log(allNodes.map(line => line.map(x => x.cost).join(" ")).join("\n"))
 console.log(allNodes[startingCoord.y][startingCoord.x].cost);
