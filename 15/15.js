b3be1e7d209384c11cdba482c7940f50f2e6cb67
Oliver Radini <orr2@live.com>
2022-12-20 00:33:12 +0000

    Part 15 finished

--- 15/15.js
+++ 15/15.js
@@ -1,25 +1,8 @@
 const { readFileSync } = require("fs");
 
 const filePath = process.argv[2] || "./data15_2";
+const scanRange = Number(process.argv[3] || "20")
 
-const zip = (a, b) => a.map((x, i) => [x, b[i]]);
-
-const range = (from, to) => {
-    try {
-        return Array.from(Array((to + 1) - from))
-            .map((_, i) => from + i);
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-const getPointsOfDistanceFromPoint = (x, y) => d => {
-    const xDiffs = range(-d, d);
-    const yDiffs = xDiffs.map(n => n < 0 ? (d - Math.abs(n)) * -1 : d - n);
-
-    return zip(xDiffs, yDiffs)
-        .map(([xD, yD]) => [x + xD, y + yD]);
-}
 
 const sensorData = readFileSync(filePath, "utf8")
     .split("\n")
@@ -36,77 +19,90 @@ const sensorData = readFileSync(filePath, "utf8")
     .map(({ sX, sY, bX, bY }) => ({
         sX, sY, bX, bY,
         distance: Math.abs(sX - bX) + Math.abs(sY - bY),
-    }))
-    // .map(s => ({
-    //     ...s,
-    //     coverage: range(0, s.distance)
-    //         .flatMap(d => getPointsOfDistanceFromPoint(s.sX, s.sY)(d))
-    // }));
+    }));
 
-// TODO: iterate all 'squares' in a row and see which aren't in the range
-//       of a sensor
+const sensorRangeCrossesCoordRange = (xMin, yMin, xMax, yMax, { sX, sY, distance }) => {
+    // at least one of the corners of the range must cross into the batch,
+    // or the batch must cross into the corner of the range
 
-const squareIsWithinABeaconsRange = (x, y) => {
-    for (let i = 0; i < sensorData.length; i++) {
-        const thisSensor = sensorData[i];
-        const {sX, sY, distance} = thisSensor;
-        const xDistanceFromSensor = Math.max(sX, x) - Math.min(sX, x);
-        const yDistanceFromSensor = Math.max(sY, y) - Math.min(sY, y);
-        const totalDistance = xDistanceFromSensor + yDistanceFromSensor;
+    const batchCorners = [
+        [xMin, yMin], [xMin, yMax], [xMax, yMin, xMax, yMax]
+    ];
 
-        if (totalDistance <= distance) {
-            // console.log(`Square (${x}, ${y}) is ${totalDistance} from sensor at (${sX}, ${y}) which has a distance of ${distance}`);
-            return true;
-        }
+    const rangeCorners = [
+        [sX - distance, sY], [sX + distance, sY], [sX, sY - distance], [sX, sY + distance]
+    ];
+
+    const someRangeCornerIsInBatch = rangeCorners.some(([x, y]) => x > xMin && x < xMax && y > yMin && y < yMax);
+    if (someRangeCornerIsInBatch) {
+        return true;
     }
 
-    return false;
-}
+    return batchCorners.some(
+        ([x, y]) => ((Math.max(sX, x) - Math.min(sX, x)) + (Math.max(sY, y) - Math.min(sY, y))) <= distance
+    );
+};
 
-const getAvailableBeaconSquares = (xFrom, xTo, y) => {
-    let takenSquares = 0;
-    for (let x = xFrom; x <= xTo; x++) {
-        // console.log(`Checking (${x}, ${y})`);
-        const thisSquareIsWithinABeaconsRange = squareIsWithinABeaconsRange(x, y)
-        if (thisSquareIsWithinABeaconsRange) {
-            takenSquares++;
-        }
+const batches = [];
+// const batchSize = 250000;
+const batchSize = scanRange / 4;
+
+for (let yStart = 0; yStart < scanRange; yStart += batchSize) {
+    console.log(`${yStart} / ${scanRange}`);
+    const yEnd = yStart + batchSize;
+    for (let xStart = 0; xStart < scanRange; xStart += batchSize) {
+        const xEnd = xStart + batchSize;
+        const theseSensors = sensorData.filter(s => sensorRangeCrossesCoordRange(
+            xStart, yStart, xEnd, yEnd, s
+        ));
+
+        batches.push({
+            xStart, xEnd, yStart, yEnd, sensors: theseSensors
+        });
     }
-
-    return takenSquares;
 }
 
-const allYCoords = sensorData.flatMap(({ sY, bY, distance }) => [sY + distance, sY - distance, bY]);
-const allXCoords = sensorData.flatMap(({ sX, bX, distance }) => [sX + distance, sX - distance, bX]);
-const minimumX = Math.min(...allXCoords);
-const minimumY = Math.min(...allYCoords);
-const maximumX = Math.max(...allXCoords);
-const maximumY = Math.max(...allYCoords);
+function* getPerimeterCoords (pX, pY, d) {
+    for (let x = -d; x <= d; x++) {
+        const thisDistance = d - Math.abs(x);
+        yield([pX + x, pY + thisDistance]);
+        yield([pX + x, pY - thisDistance]);
+    }
+}
 
-const result = getAvailableBeaconSquares(minimumX, maximumX, 2000000);
-console.log(result);
-// const groupBy = f => xs => xs.reduce((p, c) => {
-//     const thisKey = f(c);
-//     if (p.hasOwnProperty(thisKey)) {
-//         p[thisKey].push(c);
-//         return p;
-//     }
+const pointIsIsSensorRange = (pX, pY, { sX, sY, distance }) => {
+    const xDistance = Math.max(pX, sX) - Math.min(pX, sX);
+    const yDistance = Math.max(pY, sY) - Math.min(pY, sY);
+    return (xDistance + yDistance) <= distance
+}
 
-//     p[thisKey] = [c];
-//     return p;
-// }, {});
+const magicCoords = [];
+outerSensors: for (let i = 0; i < sensorData.length; i++) {
+    console.log(`${i} / ${sensorData.length}`);
+    const thisSensor = sensorData[i];
+    coords: for (let coord of getPerimeterCoords(thisSensor.sX, thisSensor.sY, thisSensor.distance + 1)) {
+        if (coord[0] < 0 || coord[0] > scanRange || coord[1] < 0 || coord[1] > scanRange) {
+            continue coords;
+        }
+        // console.log(`(${coord[0]}, ${coord[1]})`)
 
-// const mapValues = f => o => Object.keys(o)
-//     .reduce((p, c) => ({
-//         ...p,
-//         [c]: f(o[c])
-//     }));
+        if (coord[0] === 13 && coord[1] === 10) {
+            console.log("a");
+        }
+        innerSensors: for (let j = 0; j < sensorData.length; j++) {
+            if (j === i) {
+                continue innerSensors;
+            }
+            const checkSensor = sensorData[j];
+            const inSensorsRange = pointIsIsSensorRange(coord[0], coord[1], checkSensor);
+            if (inSensorsRange) {
+                continue coords;
+            }
+        }
 
-// const takeUniqueXValues = coords => Object.keys(groupBy(x => x)(coords.map(x => x[0]))).map(Number)
-// const allCoveredAreas = mapValues(takeUniqueXValues)(
-//     groupBy(x => x[1])(sensorData.flatMap(s => s.coverage))
-// );
+        // if we reached here, we know it is out of all sensors ranges
+        magicCoords.push(coord);
+    }
+}
 
-
-
-// console.log(allCoveredAreas['10']);
+console.log(magicCoords[0][0] * 4000000 + magicCoords[0][1]);
