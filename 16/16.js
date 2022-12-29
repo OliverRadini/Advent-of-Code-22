7b0d006e3381c5b8c5c1ef81e67b6c28afae366e
Oliver Radini <orr2@live.com>
2022-12-29 21:05:47 +0000

    Fix 16 code so that it gives a correct answer for part 1

--- 16/16.js
+++ 16/16.js
@@ -1,6 +1,8 @@
 const { writeFileSync, readFileSync } = require("fs");
 
-const filePath = process.argv[2] || "./test1";
+const filePath = process.argv[2] || "./data16_2";
+
+const cacheName = filePath.replace("./", "");
 
 /**
  * Valves is the data for all valves, and has raw data on
@@ -81,7 +83,7 @@ function getShortestPathBetween(from, to, alreadyVisited) {
  */
 let paths = {}
 try {
-    paths = JSON.parse(readFileSync("./paths_cache", "utf8"));
+    paths = JSON.parse(readFileSync(`./${cacheName}_cache`, "utf8"));
 } catch {
     paths = valves.filter(x => x.flowRate > 0 || x.id === "AA").reduce((p1, c1, i) => {
         console.log(`${i}/${valves.length}`);
@@ -95,7 +97,7 @@ try {
     }, {})
 }
 
-writeFileSync("./paths_cache", JSON.stringify(paths));
+writeFileSync(`./${cacheName}_cache`, JSON.stringify(paths));
 
 
 const cache = {};
@@ -109,6 +111,14 @@ const cache = {};
  */
 function bestWalkFromXWithTimeRemaining (from, timeRemaining, openNodes) {
     const key = `${from}||${timeRemaining}||${JSON.stringify(openNodes)}`;
+
+    if (from === "JJ" && timeRemaining === 5) {
+        console.log("HERE");
+    }
+
+    if (key === "EE||10||{\"AA\":false,\"BB\":true,\"CC\":true,\"DD\":true,\"EE\":false,\"FF\":false,\"GG\":false,\"HH\":true,\"II\":false,\"JJ\":false}") {
+        console.log("here");
+    }
     
     // do we already know what this is? If so, use it
     if (cache[key] !== undefined) {
@@ -118,8 +128,8 @@ function bestWalkFromXWithTimeRemaining (from, timeRemaining, openNodes) {
     // with only one minute remaining, it's not possible to affect the
     // amount of pressure released
     if (timeRemaining <= 1) {
-        cache[key] = 0;
-        return 0;
+        cache[key] = [[{from, timeRemaining, minute: 30 - timeRemaining + 1}], 0];
+        return [[{from, timeRemaining, minute: 30 - timeRemaining + 1}], 0];
     }
 
     const shouldOpenValve = rates[from] > 0 && !openNodes[from];
@@ -128,31 +138,35 @@ function bestWalkFromXWithTimeRemaining (from, timeRemaining, openNodes) {
         ? timeRemaining - 1
         : timeRemaining;
 
+    const pressureReleasedFromOpeningThisValve = shouldOpenValve
+        ? timeRemainingAfterOpeningValve * rates[from]
+        : 0;
+
     const reachableClosedNodes = Object.keys(paths[from])
         .filter(to => !openNodes[to] && paths[from][to] <= timeRemainingAfterOpeningValve);
 
     if (reachableClosedNodes.length === 0) {
-        cache[key] = 0;
-        return 0;
+        cache[key] = [[{from, timeRemaining, minute: 30 - timeRemaining + 1}], pressureReleasedFromOpeningThisValve];
+        return [[{from, timeRemaining, minute: 30 - timeRemaining + 1}], pressureReleasedFromOpeningThisValve];
     }
-
-    const pressureReleasedFromOpeningThisValve = shouldOpenValve
-        ? timeRemainingAfterOpeningValve * rates[from]
-        : 0;
 
     const allPathValues = reachableClosedNodes
         .map(to => bestWalkFromXWithTimeRemaining(
                 to,
                 timeRemainingAfterOpeningValve - (paths[from][to]),
                 { ...openNodes, [from]: shouldOpenValve || openNodes[from] }
-            
         ));
 
-    const result = pressureReleasedFromOpeningThisValve + allPathValues
+    const bestPath = allPathValues
         .reduce(
-            (p, c) => c > p ? c : p,
-            0,
+            (p, [path, pressure]) => pressure > p[1] ? [path, pressure] : p,
+            allPathValues[0],
         );
+
+    const result = [
+        [{from, timeRemaining, minute: 30 - timeRemaining + 1}, ...bestPath[0]],
+        pressureReleasedFromOpeningThisValve + bestPath[1]
+    ];
 
     cache[key] = result;
     
@@ -163,17 +177,4 @@ const allValvesClosed = valves.reduce((p, c) => ({ ...p, [c.id]: false }), {});
 const result = bestWalkFromXWithTimeRemaining("AA", 30, allValvesClosed);
 console.log(result);
 
-
-/**
- * 
- * What is going wrong?
- *   - The path is wrong?
- *   - The number is 21 short
- *     - This could be any number of things
- * 
- * 
- * What is not going wrong?
- *   - The shortest paths are all correct
- *   - The logic is at least close, as the example is correct
- *   - 
- */
+writeFileSync(`${cacheName}_results_cache`, JSON.stringify(cache, null, 4));
